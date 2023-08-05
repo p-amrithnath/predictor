233020a85f2c75e771cdf2b5fe2684be2cf2c35b
P AMRITHNATH <84299133+p-amrithnath@users.noreply.github.com>
2023-08-05 09:56:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ if switch:
     st.title('Stock Price Predictor')
     st.text("created by CECians...")
 
-    stocks = ('GOOG', 'AAPL', 'MSFT', 'GME', 'TCS.NS','AMZN')
+    stocks = ('GOOG', 'AAPL', 'MSFT', 'GME', 'TCS.NS','ADANIENT','AXISBANK','CANBK','BAJFINANCE','SUNTV','TATAMOTORS',"TVSMOTOR','VGUARD','VOLTAS','WIPRO','SIEMENS')
     selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
     n_years = st.slider('Years of prediction:', 1, 5)
